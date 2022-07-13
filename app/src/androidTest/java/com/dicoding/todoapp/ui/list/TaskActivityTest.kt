c9be24295c22413da46b6b2bfdae3891764cc515
Diandra Faiza <diandrafaiza@192.168.1.15>
2022-07-13 15:19:52 +0700

    To Do App Dicoding

---
+++ app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -0,0 +1,40 @@
+package com.dicoding.todoapp.ui.list
+
+import android.app.Activity
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.add.AddTaskActivity
+import junit.framework.TestCase.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+//TODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
+@RunWith(AndroidJUnit4ClassRunner::class)
+class TaskActivityTest {
+
+    @get:Rule
+    var activityRule = ActivityScenarioRule(TaskActivity::class.java)
+
+    @Test
+    fun testAddTaskButton(){
+        Intents.init()
+
+        onView(withId(R.id.fab))
+            .perform(click())
+        intended(hasComponent(AddTaskActivity::class.java.name))
+    }
+}
---
+++ app/src/main/java/com/dicoding/todoapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.dicoding.todoapp.utils
+
+const val TASK_ID = "TASK_ID"
+const val NOTIFICATION_CHANNEL_ID = "notify-task"
+const val NOTIFICATION_CONTENT_ID = "notify-content"
+const val NOTIFICATION_CHANNEL_ID_NUM = 102
